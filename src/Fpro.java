f1eb98d916b1e2b5ac0de84d8407e0152826da06
Kanchana Nilmini Pusweli Mulla Gamage <nilminipuswella@gmail.com>
2023-12-12 12:53:31 +0200

    Initial commit

---
+++ src/Fpro.java
@@ -0,0 +1,56 @@
+
+public class Fpro {
+//attributes
+	String brand;
+	String model;
+	String colour;
+	double weight;
+
+//creating default constructor...
+	Fpro(){}
+	
+	Fpro(String brand){
+		this.brand = brand;
+	}
+	
+	Fpro(String brand, String model, double weight){
+		this.brand = brand;
+		this.model = model;
+		this.weight = weight;
+	}
+	
+//methods...
+	public void chopping() {
+		System.out.println("Now it is chopping.");
+	}
+	
+	public void blending() {
+		System.out.println("Now it is blending.");
+	}
+	
+	public void slicing() {
+		System.out.println("Now it is slicing.");
+	}
+	
+	public void printData() {
+		System.out.println("Brand: " + brand);
+		System.out.println("Model: " + model);
+		System.out.println("Colour: " + colour);
+		System.out.println("Weight: " + weight);
+	}
+	
+	public void changeValues(String brand, String model, String colour) {
+		this.brand = brand;
+		this.model = model;
+		this.colour = colour;
+	}
+
+	
+	public static void main(String[] args) {
+		Fpro obj1 = new Fpro();
+		Fpro obj2 = new Fpro();
+		obj1.changeValues("tina", "majiv", "white");
+		obj1.printData();
+	}
+
+}
